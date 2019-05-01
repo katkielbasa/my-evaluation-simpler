1b2bca6e6f0bbb55fa535842affe2bc99632d10c
kkielbasa <kasia.kielbasa@cis.ntt.com>
2019-05-01 18:07:45 +0100

    add kendo Ui and some additional components for tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,14 +12,26 @@ import {MatMenuModule} from '@angular/material/menu';
 import {MatSidenavModule} from '@angular/material/sidenav'; 
 import {MatButtonModule} from '@angular/material/button'; 
 import {MatGridListModule} from '@angular/material/grid-list'; 
-
+import {MatDividerModule} from '@angular/material/divider'; 
+import {MatIconModule} from '@angular/material/icon'; 
+import {MatBadgeModule} from '@angular/material/badge'; 
+import {MatListModule} from '@angular/material/list'; 
 //primeng
 import {MenuModule} from 'primeng/menu';
 import {ButtonModule} from 'primeng/button';
+import {ProgressSpinnerModule} from 'primeng/progressspinner';
+import {MessagesModule} from 'primeng/messages';
+import {MessageModule} from 'primeng/message';
+import {TooltipModule} from 'primeng/tooltip';
+import {ProgressBarModule} from 'primeng/progressbar';
+import {CaptchaModule} from 'primeng/captcha';
+import {ToastModule} from 'primeng/toast';
 //nebular
 import { NbThemeModule, NbLayoutModule, NbMenuModule, NbButtonModule, NbCardModule } from '@nebular/theme';
 //ngbootstrap
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+//ngx-bootstrao
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 //ngzorro
 import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
 import { FormsModule } from '@angular/forms';
@@ -29,6 +41,12 @@ import en from '@angular/common/locales/en';
 
 //ng-lightening
 import {NglModule, NGL_ICON_CONFIG, NglIconConfig} from 'ng-lightning';
+//KENDO
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { LayoutModule } from '@progress/kendo-angular-layout';
+import { MenusModule } from '@progress/kendo-angular-menu';
+
+
 
 registerLocaleData(en);
 
@@ -41,12 +59,24 @@ registerLocaleData(en);
     BrowserModule,
     ClarityModule,
     MatGridListModule,
+    MatIconModule,
     BrowserAnimationsModule,
     NgbModule,
     NglModule,
+    MatBadgeModule,
+    ProgressSpinnerModule,
+    MessagesModule,
+    MessageModule,
+    TooltipModule,
+    ProgressBarModule,
+    CaptchaModule,
+    ToastModule,
+    MatListModule,
     // routing,
     MenuModule,
+    MenusModule,
     MatMenuModule,
+    MatDividerModule,
     MatSidenavModule,
     MatButtonModule,
     ButtonModule,
@@ -54,11 +84,14 @@ registerLocaleData(en);
     NbLayoutModule,
     NbButtonModule,
     NbMenuModule.forRoot(),
+    BsDropdownModule.forRoot(),
     NbCardModule,
     AppRoutingModule,
     NgZorroAntdModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ButtonsModule,
+    LayoutModule
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US },
     { provide: NGL_ICON_CONFIG, useValue: <NglIconConfig>{ svgPath: '/my/path' }}],
